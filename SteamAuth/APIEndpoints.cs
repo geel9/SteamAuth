23fe68bbeea43e1d54e95aa0a2048675020d9192
geel9 <coffeyjosh1@gmail.com>
2015-11-26 01:50:30 -0500

    Add APIEndpoints.cs

---
+++ SteamAuth/APIEndpoints.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamAuth
+{
+    public static class APIEndpoints
+    {
+        public static string STEAMAPI_BASE = "https://api.steampowered.com";
+        public static string TWO_FACTOR_BASE = STEAMAPI_BASE + "/ITwoFactorService/%s/v0001";
+        public static string TWO_FACTOR_TIME_QUERY = TWO_FACTOR_BASE.Replace("%s", "QueryTime");
+    }
+}
