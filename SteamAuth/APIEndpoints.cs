b3babd07aaabcbe1e633cde9eda1a335ddf1bbd5
geel9 <coffeyjosh1@gmail.com>
2015-11-26 03:52:19 -0500

    Rework APIEndpoint.cs

--- SteamAuth/APIEndpoints.cs
+++ SteamAuth/APIEndpoints.cs
@@ -8,8 +8,9 @@ namespace SteamAuth
 {
     public static class APIEndpoints
     {
-        public static string STEAMAPI_BASE = "https://api.steampowered.com";
-        public static string TWO_FACTOR_BASE = STEAMAPI_BASE + "/ITwoFactorService/%s/v0001";
+        public const string STEAMAPI_BASE = "https://api.steampowered.com";
+        public const string COMMUNITY_BASE = "https://steamcommunity.com";
+        public const string TWO_FACTOR_BASE = STEAMAPI_BASE + "/ITwoFactorService/%s/v0001";
         public static string TWO_FACTOR_TIME_QUERY = TWO_FACTOR_BASE.Replace("%s", "QueryTime");
     }
 }
