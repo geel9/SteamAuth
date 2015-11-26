7c94fe641e88630837f941b5598ca60f340eb1ae
geel9 <coffeyjosh1@gmail.com>
2015-11-26 03:55:46 -0500

    Clean up usings

--- SteamAuth/UserLogin.cs
+++ SteamAuth/UserLogin.cs
@@ -1,12 +1,9 @@
 ﻿using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.Collections.Specialized;
-using System.Linq;
 using System.Net;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace SteamAuth
 {
--- TestBed/Program.cs
+++ TestBed/Program.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using SteamAuth;
-using System.Security.Cryptography;
 
 namespace TestBed
 {
